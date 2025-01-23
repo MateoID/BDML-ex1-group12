5350f216e931af0cd3083a2a68697ed1e6389a63
lialvareza1 <lm.alvareza1@uniandes.edu.co>
2025-01-22 19:27:55 -0500

    Update impute2.R

--- impute2.R
+++ impute2.R
@@ -1,1 +1,14 @@
 # Include your code here for you second imputation method
+
+if(!require(pacman)) install.packages("pacman") ; require(pacman)
+# require/install packages on this session
+p_load(rio, # import/export data
+       tidyverse, # tidy-data
+       skimr, # summary data
+       visdat, # visualizing missing data
+       corrplot, # Correlation Plots 
+       stargazer) # tables/output to TEX. 
+        
+## load data
+df <- import("https://github.com/ignaciomsarmiento/datasets/blob/main/GEIH_sample1.Rds?raw=true")
+db <- as_tibble(df) ## from dataframe to tibble
