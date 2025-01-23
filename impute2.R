8133b14d7b3243ab9b11e233386d15cfdbc3a6e4
lialvareza1 <lm.alvareza1@uniandes.edu.co>
2025-01-22 19:39:16 -0500

    Update impute2.R

--- impute2.R
+++ impute2.R
@@ -1,15 +1,4 @@
-if(!require(pacman)) install.packages("pacman") ; require(pacman)
-# require/install packages on this session
-p_load(rio, # import/export data
-       tidyverse, # tidy-data
-       skimr, # summary data
-       visdat, # visualizing missing data
-       corrplot, # Correlation Plots 
-       stargazer) # tables/output to TEX. 
 
-## load data
-df <- import("https://github.com/ignaciomsarmiento/datasets/blob/main/GEIH_sample1.Rds?raw=true")
-db <- as_tibble(df)
 db$maxEducLevel<- factor(db$maxEducLevel)
 
 dummy_maxEducLevel <- as.data.frame(model.matrix(~ maxEducLevel - 1, data = db)) 
